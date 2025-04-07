7300547ad4383230226d920692857587afdd8e3f
TheTrackLP <113531678+TheTrackLP@users.noreply.github.com>
2025-04-07 13:14:47 +0800

    Report Module, still need sa small changes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Borrower;
+use App\models\LoanPlans;
+use App\models\Payments;
+use DB;
 
 class ReportController extends Controller
 {
     public function ReportLoan()
     {
-        return view('admin.backend.report.reports');
+        $plans = LoanPlans::all();
+        $payees = Payments::select(
+            'payments.*',
+            'loan_plans.*',
+            DB::raw("CONCAT(borrowers.lastname, ', ', borrowers.firstname, ' ', borrowers.middlename) as borrow"),
+            DB::raw("SUM(payments.capital) as total_capital, SUM(payments.paid) as total_paid, SUM(payments.interest) as total_interest"),
+        )
+        ->join("borrowers", "borrowers.id", '=', 'payments.borrower_id')
+        ->join("loan_plans", "loan_plans.id", '=', 'payments.plan_id')
+        ->groupBy("payments.borrower_id", "payments.plan_id")
+        ->get()
+        ->groupBy("borrower_id");
+        return view('admin.backend.report.reports', compact('plans', 'payees'));
     }
 }
--- resources/views/admin/backend/report/reports.blade.php
+++ resources/views/admin/backend/report/reports.blade.php
@@ -15,18 +15,50 @@
             <table class="table table-bordered" id="reportTable">
                 <thead class="table-info">
                     <tr>
-                        <th class="text-center" rowspan="2">#</th>
-                        <th class="text-center" rowspan="2">Month Year</th>
-                        <th class="text-center">Plan</th>
-                        <th class="text-center" role="2">Paid-In</th>
-                        <th class="text-center" role="2">Other Receivable</th>
-                        <th class="text-center" role="2">Total</th>
+                        <th class="text-center align-middle" rowspan="2">#</th>
+                        <th class="text-center align-middle" rowspan="2">Month Year</th>
+                        @foreach ($plans as $plan)
+                        <th class="text-center" colspan="2">{{ $plan->plan_name }}</th>
+                        @endforeach
+                        <th class="text-center" rowspan="2">Paid-In</th>
+                        <th class="text-center" rowspan="2">Penalty</th>
+                        <th class="text-center" rowspan="2">Total</th>
                     </tr>
                     <tr>
+                        @foreach ($plans as $plan)
                         <th class="text-center">PRIN</th>
                         <th class="text-center">INT</th>
+                        @endforeach
                     </tr>
                 </thead>
+                <tbody>
+                    @php
+                        $i = 1;
+                    @endphp
+                    @foreach ($payees as $borrower_id => $payee)
+                    <tr>
+                        <td class="text-center">{{ $i++ }}</td>
+                        <td>{{ $payee->first()->borrow }}</td>
+                        @foreach ($plans as $plan)
+                        @php
+                            $payments = $payee->firstWhere("plan_id", $plan->id);
+                        @endphp
+                        @if ($payments)
+                            <td class="text-center">{{ number_format($payments->paid,2 ) }}</td>
+                            <td class="text-center">{{ $payments->interest ? number_format($payments->interest,2 ) : '' }}</td>
+                        @else
+                        <td class="text-center"></td>
+                        <td class="text-center"></td>
+                        @endif
+                        @endforeach
+                        <td class="text-center">{{ $payee->sum('penalty') ? number_format($payee->sum('penalty'),2 ) : ''}}</td>
+                        <td class="text-center">{{ $payee->sum('tota_capital') ? number_format($payee->sum('tota_capital'),2 ) : '' }}</td>
+                        <td class="text-center">
+                            {{ number_format($payee->sum('paid') + $payee->sum('interest') + $payee->sum('tota_capital') + $payee->sum('penalty'),2 ) }}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
             </table>
         </div>
     </div>
